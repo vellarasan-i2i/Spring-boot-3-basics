2b7e01c7029f2f3fcb95bbcec08ec108bbff6a27
Vellarasan Dhanapathy <vdhanapathy@ntst.com>
2022-12-04 10:35:22 +0530

    Initial commit

---
+++ src/main/java/io/vels/springboot3basics/SpringBoot3BasicsApplication.java
@@ -0,0 +1,13 @@
+package io.vels.springboot3basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3BasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3BasicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/vels/springboot3basics/SpringBoot3BasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vels.springboot3basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3BasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
